7a7fa12fd2b47b55742dfcb46884b70c02f3dd04
Timea Bezdan <39801350+Timi01@users.noreply.github.com>
2018-11-27 00:24:01 +0100

    Update MinimumAndMaximum.java

--- MinimumAndMaximum.java
+++ MinimumAndMaximum.java
@@ -6,8 +6,8 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
 
         int counter = 0;
-        int min = Integer.MIN_VALUE; // or int min = 0;
-        int max = Integer.MAX_VALUE; // or int max = 0;
+        int min = Integer.MIN_VALUE; 
+        int max = Integer.MAX_VALUE; 
 
         while (counter < 10) {
             int order = counter + 1;
